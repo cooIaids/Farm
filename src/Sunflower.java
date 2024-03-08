77f9c619f39ce839262ef729ea39c5bfa362bbb5
borisova <borisova.marie1@gmail.com>
2024-03-08 12:57:57 +0100

    Added classes Sunflower and Tulip

---
+++ src/Sunflower.java
@@ -0,0 +1,6 @@
+public class Sunflower extends Flower{
+
+    public Sunflower(String name, double price, double neededArea, double chanceOfGrowth) {
+        super(name, price, neededArea, chanceOfGrowth);
+    }
+}
---
+++ src/Tulip.java
@@ -0,0 +1,6 @@
+public class Tulip extends Flower{
+
+    public Tulip(String name, double price, double neededArea, double chanceOfGrowth) {
+        super(name, price, neededArea, chanceOfGrowth);
+    }
+}
