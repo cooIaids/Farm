f8256f9337fa1b152afb14617cc3147278021eba
borisova <borisova.marie1@gmail.com>
2024-03-08 12:47:41 +0100

    Added class Cow and constructor to class Animal

---
+++ src/Cow.java
@@ -0,0 +1,6 @@
+public class Cow extends Animal{
+
+    public Cow(String type, String name, Size size, String specialCare) {
+        super(type, name, size, specialCare);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Cow cow = new Cow("cow","Betty", Size.BIG, "feed every 2 minutes");
+        System.out.println(cow);
     }
 }
